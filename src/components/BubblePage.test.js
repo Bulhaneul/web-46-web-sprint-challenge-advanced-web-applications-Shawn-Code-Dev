dee32abc12269a4c2c2ee49adaaac4447ae05c4c
Shawn Rice <Shawn.CS@protonmail.com>
2021-09-17 19:25:23 -0400

    did test I forgot to do

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import MutationObserver from 'mutationobserver-shim';
 
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import BubblePage from './BubblePage';
 import fetchColorService from '../services/fetchColorService';
 jest.mock('../services/fetchColorService')
+const mockData = []
 
 test("Renders without errors", ()=> {
     render(<BubblePage />)
 });
 
 test("Renders appropriate number of colors passed in through mock", async ()=> {
+    fetchColorService.mockResolvedValueOnce(mockData)
     //Keep in mind that our service is called on mount for this component.
-    
     render(<BubblePage />)
-    
+    await waitFor(() => expect(screen.queryAllByTestId("color")).toHaveLength(0))    
 });
